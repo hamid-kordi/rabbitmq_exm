6d639c47633c7216c679ee45ce55bdaa4a4ff718
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-05-03 09:27:35 +0330

    feat:add server rpc

--- 6/client.py
+++ 6/client.py
@@ -8,18 +8,25 @@ class Sender:
         self.ch = self.connection.channel()
         result = self.ch.queue_declare(queue='',exclusive=True)
         self.queuename = result.method.queue
+        self.ch.basic_consume(queue=self.queuename,on_message_callback=self.on_response,auto_ack=True)
 
-
+    def on_response(self,ch,method,proper,body):
+        if self.corr_id == proper.correlation_id:
+            self.response = body
+        
     def call(self,n):
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.ch.basic_publish(exchange='',routing_key='rpc_queue',
                               properties=pika.BasicProperties(reply_to=self.queuename,correlation_id=self.corr_id),body=str(n))
 
-        pass
+        while self.response is None:
+            self.connection.process_data_events()
+        return int(self.response)
 
 
 
 send = Sender()
 
-send.call(30)
+
+print(send.call(30))
--- 6/server.py
+++ 6/server.py
@@ -4,7 +4,6 @@ import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
 ch = connection.channel()
-
 ch.queue_declare(queue="rpc_queue")
 
 
@@ -13,8 +12,15 @@ def callback(ch, method, proper, body):
     print("work on masseges")
     time.sleep(3)
     response = n + 1
+    ch.basic_publish(
+        exchange="",
+        routing_key=proper.reply_to,
+        properties=pika.BasicProperties(correlation_id=proper.corolation_id),
+        body=str(response),
+    )
+    ch.basic_ack(delivery_tag = method.delivery())
 
-
-ch.basic_consume(queue='',on_message_callback=callback)
-print('wait for messages')
+ch.basic_qos(prefetch_count=1)
+ch.basic_consume(queue="", on_message_callback=callback)
+print("wait for messages")
 ch.start_consuming()
